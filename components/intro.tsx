362c5a0e011596971c87422ce9e93329fb853541
viren1610 <virenplays@gmail.com>
2023-12-28 17:27:30 +0530

    added resume

--- components/intro.tsx
+++ components/intro.tsx
@@ -80,7 +80,7 @@ export default function Intro() {
         </Link>
         <a className="group bg-white px-7 py-3 flex items-center gap-2 
         rounded-full outline-none focus:scale-110 hover:scale-110 
-        active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10">
+        active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" href="/VirenWalia.pdf" download>
           Download Resume{" "}
           <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
